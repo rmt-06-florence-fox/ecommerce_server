9e8f4cdfdbf3f34e73a59cb7b5fa6bd73dfd95d5
icanq <icanq@.localdomain>
2020-12-07 17:27:26 +0700

    first initiate server

---
+++ server.js
@@ -0,0 +1,9 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const http = require("http");
+const app = require("./app");
+const PORT = process.env.PORT || 4000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
