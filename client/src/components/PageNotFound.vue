71d785dc79016e5fe4717147a53c49e47c90ae7d
adhim1st <adhim@student.uns.ac.id>
2020-12-09 17:43:36 +0700

    navigation guard login and 404 page

---
+++ client/src/components/PageNotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <center>
+    <h1>Error 404 Page Not Found</h1>
+  </center>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../views/Login.vue";
 import LandingPage from "../views/LandingPage.vue";
+import PageNotFound from "../components/PageNotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -9,19 +10,32 @@ const routes = [
   {
     path: "/login",
     name: "Login",
-    component: Login
+    component: Login,
   },
   {
     path: "/landing-page",
     name: "Landing Page",
-    component: LandingPage
-  }
+    component: LandingPage,
+  },
+  {
+    path: "*",
+    name: "Page Not Found",
+    component: PageNotFound,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.access_token;
+  if (to.name !== "Login" && !isAuthenticated) next({ name: "Login" });
+  else if (to.name == "Login" && isAuthenticated)
+    next({ name: "Landing Page" });
+  else next();
 });
 
 export default router;
