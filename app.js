3a9af3fb31d3b01de63140d59b89fde5bc401d53
hndrbudisantoso@gmail.com <hndrbudisantoso@gmail.com>
2020-12-09 23:08:32 +0700

    fixing blocks by CORS

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ if(process.env.NODE_ENV != 'production'){
 const errorHandler = require('./middlewares/error-handler.js')
 const express = require('express')
 const router = require('./routers')
-
 const app = express()
+const cors = require('cors')
 
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use('/' , router)
-
 app.use(errorHandler)
 
 module.exports = app
