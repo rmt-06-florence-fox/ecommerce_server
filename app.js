433748d977f98e9d93263090da1d641bbaf9b052
muchsin28 <hab@muchsin.id>
2020-12-10 18:07:49 +0700

    ready to deploy ; '

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 const router = require('./routes')
 const cors = require('cors')
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(router)
