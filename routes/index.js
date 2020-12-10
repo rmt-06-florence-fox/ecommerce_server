54c2f05fe7eaf61f78ae2b4961fd00ff772935c9
RizkyMaulita <rizky.maulita@gmail.com>
2020-12-10 09:05:12 +0700

    salah penaruhan tempat route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router()
 const UserController = require('../controllers/userController')
 const productRouter = require('./productRouter')
-const ProductController = require('../controllers/productController')
 
-router.get('/products', ProductController.getAllProduct)
 router.post('/login', UserController.loginAdmin)  
 //--- ditambah login cust, regist cust
 router.use('/products', productRouter)
