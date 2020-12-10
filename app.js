1d16ce7cf9f973eaaaec3e0da96441706ef8cab4
sidsoeharto <msidiq.15@gmail.com>
2020-12-10 13:56:31 +0700

    update cors

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require('express');
 const app = express();
-//const cors = require('cors');
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 const router = require('./routers');
 const errorHandler = require('./middlewares/errorHandler');
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
