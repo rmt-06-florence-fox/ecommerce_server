e5940bb4a6906aa8e2e98ccc0fdc4cc87193e6e4
aldidestan <destaodervon@gmail.com>
2020-12-10 08:09:29 +0700

    update folder

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+if(process.env.NODE_ENV != 'production'){
+  require('dotenv').config()
+}
 const http = require('http')
 const app = require('./app')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('port', port)
 
