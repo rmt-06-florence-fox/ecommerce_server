a251a407f1b65a7e0042f8914d3e91e31b261dfa
Andrenoj11 <andrenojuanda94@gmail.com>
2020-12-12 21:06:42 +0700

    ready to deploy2

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express()
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
-const http = require('http')
-const server = http.createServer(app)
-const PORT = 3000
+// const http = require('http')
+// const server = http.createServer(app)
+// const PORT = 3000
 
 app.use(cors())
 
@@ -20,7 +20,7 @@ app.use(routes)
 
 app.use(errorHandler)
 
-server.listen(PORT, () => {
-    console.log('listen on port ', PORT );
-})
+// server.listen(PORT, () => {
+//     console.log('listen on port ', PORT );
+// })
 module.exports = app
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http')
 const app = require('./app')
-const PORT = 3000
+const port = process.env.PORT || 3000
 
 const server = http.createServer(app)
 
 server.listen(PORT, () => {
-    console.log('listen on port ', PORT );
+    console.log('listen on port ', port );
 })
