7a9e2b9631896a39a75f306da46bba2fd297d60c
YuniMarlina <yunimmarlina@gmail.com>
2020-12-12 15:11:45 +0700

    lastest-update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-const app = require('./app')
-const http = require('http')
-const port = 3000
-
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+const http = require("http")
+const app = require("./app")
+const port = process.env.PORT || 3000
 const server = http.createServer(app)
 
 server.listen(port, () => {
