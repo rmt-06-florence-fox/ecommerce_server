94d66f37ccef4ee1108fdf2e4e4ac7404da42361
Andrenoj11 <andrenojuanda94@gmail.com>
2020-12-12 21:04:30 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express()
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
+const http = require('http')
+const server = http.createServer(app)
+const PORT = 3000
 
 app.use(cors())
 
@@ -17,4 +20,7 @@ app.use(routes)
 
 app.use(errorHandler)
 
+server.listen(PORT, () => {
+    console.log('listen on port ', PORT );
+})
 module.exports = app
