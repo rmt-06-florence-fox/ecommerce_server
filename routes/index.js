a1c2aa90f8d23b98ea3e95baa8138b929d107623
hafis915 <hafistafsani915uh@gmail.com>
2020-12-12 18:34:11 +0800

    deploy

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const routes = require('express').Router()
 const productRoutes = require('./productRoutes')
 const userRoutes = require('./userRoutes')
 
-
+routes.get('/', (req,res) =>{
+    res.send('hello')
+})
 routes.use('/',userRoutes)
 routes.use('/product',productRoutes)
 
