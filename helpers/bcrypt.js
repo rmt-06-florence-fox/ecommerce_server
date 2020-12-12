2a156ade77ec4b7873de6d3ef63b567cd2a25b77
Hisyam-Khowarik <hisyamkhowarik@gmail.com>
2020-12-12 21:22:08 +0700

    updated

--- helpers/bcrypt.js
+++ helpers/bcrypt.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs')
 
 function hashPass(password) {
-    let salt = bcrypt.genSaltSync(process.env.SECRET);
+    let salt = bcrypt.genSaltSync(10);
     let hash = bcrypt.hashSync(password, salt)
     return hash
 }
