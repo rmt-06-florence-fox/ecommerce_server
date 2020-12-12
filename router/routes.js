8221263e5b0f566643f354947430818cf9204119
aldidestan <destaodervon@gmail.com>
2020-12-12 13:44:56 +0700

    update deploy

--- router/routes.js
+++ router/routes.js
@@ -5,9 +5,9 @@ const authentication = require('../middlewares/authentication')
 const authorization = require('../middlewares/authorization')
 
 router.post('/signin', UserControl.signin)
-router.get('/products', ProductControl.showData)
 router.use(authentication)
-router.post('/products', ProductControl.create)
+router.get('/products', ProductControl.showData)
+router.post('/products', authorization, ProductControl.create)
 router.put('/products/:id', authorization, ProductControl.edit)
 router.delete('/products/:id', authorization, ProductControl.deleteData)
 
