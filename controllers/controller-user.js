ac4709dd3208b47a7a3d0b7511c216f9989f833f
wypratama <wicaksanapratama@gmail.com>
2020-12-12 15:28:17 +0700

    dev-add-user-routing-to-get-all-user

--- controllers/controller-user.js
+++ controllers/controller-user.js
@@ -65,7 +65,16 @@ class ControllerUser {
         try {
             
         } catch (err) {
-            
+            next(err)
+        }
+    }
+
+    static async getAll (req, res, next) {
+        try {
+            const data = await User.findAll()
+            res.status(200).json(data)
+        } catch (err) {
+            next(err)
         }
     }
 
--- routers/router-user.js
+++ routers/router-user.js
@@ -1,12 +1,15 @@
 const user = require ('express').Router()
 const { ControllerUser } = require ('../controllers')
 const authentication = require ('../middlewares/authentication')
+const adminAuth = require ('../middlewares/admin-authorization')
 
 
 user.get('/', ControllerUser.welcome)
 user.post('/login', ControllerUser.login)
 user.post('/register', ControllerUser.register)
-user.get('/user', authentication, ControllerUser.getUser)
+user.get('/user', authentication, adminAuth, ControllerUser.getUser)
+user.get('/user/all', authentication, adminAuth, ControllerUser.getAll)
+
 
 
 module.exports = user
