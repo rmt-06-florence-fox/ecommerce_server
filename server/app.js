6afb18b3cbf4e6e5582c41aa7969088f99ae11b5
YuniMarlina <yunimmarlina@gmail.com>
2020-12-12 14:54:43 +0700

    lastest-update

--- server/app.js
+++ server/app.js
@@ -10,12 +10,9 @@ const cors = require('cors')
 const errorHandler = require('./middlewares/errorHandler')
 
 
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors())
-
-
 
 app.use(router)
 app.use(errorHandler)
