d198061a38d39adb1b65130c02ad1c10076a0a4b
hafis915 <hafistafsani915uh@gmail.com>
2020-12-12 18:14:09 +0800

    config untuk deploy ke heroku

--- helper/jwt.js
+++ helper/jwt.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken')
 
 function getToken(payload){
-    const token = jwt.sign(payload , 'barca')
+    const token = jwt.sign(payload , process.env.secret)
     return token
 }
 
 
 async function hashToken(token){
     try {
-        const decode = await jwt.verify(token, 'barca')
+        const decode = await jwt.verify(token, process.env.secret)
         return decode
     } catch (error) {
         throw {
