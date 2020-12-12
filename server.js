1499a4a0c7b505d41bbe2ab367cbe431dcdc6843
Prasetya Rinaldi <64371340+prasetyarinaldii@users.noreply.github.com>
2020-12-12 19:53:03 +0700

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require("http")
 const app = require("./app")
 const server = http.createServer(app)
-const PORT = 3000
-
+const PORT = process.env.PORT || 3000 
 
 server.listen(PORT, () => {
     console.log(`runing in port: ${PORT}`);
