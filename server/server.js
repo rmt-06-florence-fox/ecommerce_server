58b1bb37b3cdb4a64cc6a88534ad387ca4d59c0b
YuniMarlina <yunimmarlina@gmail.com>
2020-12-13 21:32:10 +0700

    latest update

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const app = require("./app")
 const port = process.env.PORT || 3000
 const server = http.createServer(app)
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
