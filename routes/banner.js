13338ef81e59f67325d956e7395d1287e9337783
taufikikk <taufikikk@gmail.com>
2020-12-15 14:30:48 +0700

    add authentication in banner routes

--- routes/banner.js
+++ routes/banner.js
@@ -1,7 +1,9 @@
 const route = require('express').Router()
 const {BannerController} = require('../controllers')
 const authAdmin = require('../middlewares/authAdmin')
+const authentication = require('../middlewares/authentication')
 
+route.use(authentication)
 route.get('/', authAdmin, BannerController.findAllBanner)
 route.get('/:id', authAdmin, BannerController.findByIdBanner)
 route.post('/', authAdmin, BannerController.addBanner)
