ba41024b26c05c323ef8808ad1d5b4105d65b03d
tiorzal <tioralmunazir@gmail.com>
2020-12-15 20:42:32 +0700

    update
    udah bikin table cart
    rapihin fk di migration
    rapihin asisosiasi di model
    next mau bikin routing

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const {UserController} = require('../controllers')
 
+//punya admin
 router.post('/login', UserController.login)
 router.post('/register', UserController.register)
+//punya customer
 router.post('/logincustomer', UserController.loginCustomer)
 
 
