0cd678a8c3acd1001be890bd085667913cbd7d61
ogyrahmawan <ogyrahmawan@gmail.com>
2020-12-16 12:14:50 +0700

    revisi

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,6 @@ class UserController {
     }
     static async login(req, res, next){
         try {
-            console.log('masuk sini kan???')
             let data = await User.findOne({
                 where: {
                     email: req.body.email    
@@ -33,15 +32,13 @@ class UserController {
                 if(data.role == 'admin'){
                     if(compare(req.body.password, data.password)){
                         const access_token = generateToken({id: data.id, email: data.email, role: data.role})
-                        res.status(200).json({
-                            access_token
-                        })
+                        res.status(200).json({ access_token })
+                    }
                 } else {
                     throw({
                         status: 400,
-                        message: `invalid email or password`
+                        message: `You dont have access`
                     })
-                }
                 }
             }
         } catch (error) {
