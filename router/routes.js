315c7a63a237e52c508df57d30810b5b220ca869
aldidestan <destaodervon@gmail.com>
2020-12-17 12:26:40 +0700

    update deploy

--- router/routes.js
+++ router/routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const UserControl = require('../controller/user')
 const ProductControl = require('../controller/product')
-const BannerControl = require('../controller/banner')
 const cartrouter = require('./cartsrouter')
 const {adminAuthentication} = require('../middlewares/authentication')
 const authorization = require('../middlewares/authorization')
@@ -10,7 +9,6 @@ router.post('/signin', UserControl.signin)
 router.post('/signup', UserControl.signup)
 
 router.get('/products', ProductControl.showData)
-router.get("/banners", BannerControl.showBanner)
 router.use("/carts", cartrouter)
 
 router.use(adminAuthentication)
@@ -20,10 +18,5 @@ router.get('/products/:id', ProductControl.getDataById)
 router.put('/products/:id', authorization, ProductControl.edit)
 router.delete('/products/:id', authorization, ProductControl.deleteData)
 
-
-router.post("/banners", authorization, BannerControl.addBanner)
-router.put("/banners/:id", authorization, BannerControl.updateBanner)
-router.patch("banners/:id", authorization, BannerControl.updateStatus) 
-router.delete("/banners/:id",authorization, BannerControl.deleteBanner)
 
 module.exports = router
